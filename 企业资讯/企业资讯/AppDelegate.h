6388d3cb275185691c40282fd9f8955010644157
chenjie <chenjie@tantekiMac-mini.local>
2014-12-26 14:36:05 +0800

    新建项目

---
+++ 企业资讯/企业资讯/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  企业资讯
+//
+//  Created by chenjie on 14/12/26.
+//  Copyright (c) 2014年 winhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 企业资讯/企业资讯/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  企业资讯
+//
+//  Created by chenjie on 14/12/26.
+//  Copyright (c) 2014年 winhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
